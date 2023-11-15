d49c604874b4e7d814674a0985680fc76d3bb4b2
Blaise <jetblaise@gmail.com>
2023-11-15 17:58:19 -0500

    fixed linting and footer

--- components/icons/logo.tsx
+++ components/icons/logo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function LogoIcon(props: React.SVGProps<SVGSVGElement>) {
+export default function LogoIcon(_props: React.SVGProps<SVGSVGElement>) {
   return (
     <>
       <svg
