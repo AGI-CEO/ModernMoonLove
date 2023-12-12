8323186925758579ae9663de04d1959cba7afcba
Blaise <jetblaise@gmail.com>
2023-12-11 20:35:44 -0500

    Refactor logo-square component and update hero background size

--- components/logo-square.tsx
+++ components/logo-square.tsx
@@ -3,11 +3,13 @@ import LogoIcon from './icons/logo';
 
 export default function LogoSquare({ size }: { size?: 'sm' | undefined }) {
   return (
-    <LogoIcon
+    <div
       className={clsx({
-        'h-[80px] w-[80px] rounded border': !size,
-        'h-[20px] w-[20px] rounded border': size === 'sm'
+        'flex h-[80px] w-[80px] items-center justify-center rounded border': !size,
+        'flex h-[20px] w-[20px] items-center justify-center rounded border': size === 'sm'
       })}
-    />
+    >
+      <LogoIcon className="object-contain" />
+    </div>
   );
 }
